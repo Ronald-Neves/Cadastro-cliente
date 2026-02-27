a1d6e69abaca79de28ca7ef83b69b89554b9ff4b
Ronald_Neves <ronaldnevesbailon@gmail.com>
2026-02-27 15:55:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-script.js
 const form = document.getElementById("formCliente");
 const lista = document.getElementById("listaClientes");
+
 form.addEventListener("submit", function(event) {
- event.preventDefault();
- const nome = document.getElementById("nome").value;
- const email = document.getElementById("email").value;
- const li = document.createElement("li");
- li.textContent = `${nome} - ${email}`;
- lista.appendChild(li);
- form.reset();
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+
+    // 🔹 Validação simples de email
+    if (!email.includes("@")) {
+        alert("Email inválido! Digite um email com @");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = `${nome} - ${email}`;
+
+    lista.appendChild(li);
+
+    form.reset();
 });
