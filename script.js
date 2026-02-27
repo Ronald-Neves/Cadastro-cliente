5702e784d1203d6398d428204c50ae2f70dd0a93
Ronald Neves <ronaldnevesbailon@gmail.com>
2026-02-27 15:45:16 -0300

    Projeto inicial cadastro clientes

---
+++ script.js
@@ -0,0 +1,12 @@
+script.js
+const form = document.getElementById("formCliente");
+const lista = document.getElementById("listaClientes");
+form.addEventListener("submit", function(event) {
+ event.preventDefault();
+ const nome = document.getElementById("nome").value;
+ const email = document.getElementById("email").value;
+ const li = document.createElement("li");
+ li.textContent = `${nome} - ${email}`;
+ lista.appendChild(li);
+ form.reset();
+});
